f75a6c27995ed52973d3add6ea67ed54f1e23505
Metin Yalçınkaya <metin.ylnky00@gmail.com>
2019-07-15 15:20:51 +0300

    Add files via upload

---
+++ search_slnm.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from getpass import getpass
+import time
+
+enterword=input("Enter word: ")
+#Firefox: webdriver.Firefox() - Chrome: webdriver.Chrome()
+browser=webdriver.Firefox(executable_path=r"gecko_driver_path")
+browser.get('https://www.website.com')
+
+time.sleep(3)
+username=browser.find_element_by_xpath("website_input_xpath")
+username.send_keys(enterword)
+time.sleep(1)
+login=browser.find_element_by_name("web_site_btn_name")
+login.click()
+time.sleep(5)
+browser.quit()
